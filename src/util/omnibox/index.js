24082eef76e761f24ddefe56f752da78cc5fe79e
MaPl <mapl@york.moe>
2022-03-01 19:28:05 +0800

    feature: 集成地址栏建议功能

--- src/util/omnibox/index.js
+++ src/util/omnibox/index.js
@@ -1,30 +1,55 @@
-class omnibox {
-    constructor() {
-        // this.inputString = ''
-        // browser.omnibox.onInputStarted.addListener(this._onInputStarted)
-        // browser.omnibox.onInputChanged.addListener(this._onInputChanged)
-        // browser.omnibox.onInputEntered.addListener(this._onInputEntered)
-        // browser.omnibox.onInputCancelled.addListener(this._onInputCancelled)
-    }
-    _onInputStarted() {
-        browser.omnibox.setDefaultSuggestion({
-            description: '快速访问兰州大学相关网站'
+const { distance } = require('fastest-levenshtein')
+
+browser.omnibox.setDefaultSuggestion({
+    description: "在百度搜索"
+})
+
+const urls = [
+    {"name": "信息科学与工程学院", "url": "http://xxxy.lzu.edu.cn"},
+    {"name": "网络安全与信息化办公室", "url": "http://its.lzu.edu.cn"},
+    {"name": "个人工作台", "url": "http://my.lzu.edu.cn"},
+    {"name": "邮箱", "url": "https://mail.lzu.edu.cn"},
+    {"name": "校园网登录", "url": "http://login.lzu.edu.cn"},
+    {"name": "校园一卡通", "url": "http://ecard.lzu.edu.cn"},
+    {"name": "开源社区", "url": "http://oss.lzu.edu.cn"},
+    {"name": "镜像站", "url": "http://mirrors.lzu.edu.cn"},
+    {"name": "教务处", "url": "http://jwc.lzu.edu.cn"},
+    {"name": "教务系统", "url": "http://jwk.lzu.edu.cn"}
+]
+
+browser.omnibox.onInputChanged.addListener((text, suggest) => {
+    const url_list = urls.map(e => ({
+        name: e.name,
+        distance: distance(text, e.name),
+        url: e.url
+    })).sort((a, b) => a.distance - b.distance).slice(0, 5)
+    suggest(url_list.map(e => ({
+        content: e.url,
+        description: e.name
+    })))
+})
+
+browser.omnibox.onInputEntered.addListener((text, disposition) => {
+    let url = "about:black"
+    if (!text.startsWith("http://")) {
+        const wd = new URLSearchParams({
+            wd: "兰州大学 " + text
         })
+        url = "https://www.baidu.com/baidu?" + wd.toString()
+    } else {
+        url = text
     }
-    _onInputChanged(text, suggest) {
-        suggest([{
-            content: 'http://www.lzu.edu.cn',
-            description: '兰州大学'
-        },{
-            content: 'http://www.lzu.edu.cn',
-            description: text
-        }])
+    switch (disposition) {
+        case "currentTab":
+            browser.tabs.update({url})
+            break
+        case "newForegroundTab":
+            browser.tabs.create({url})
+            break
+        case "newBackgroundTab":
+            browser.tabs.create({url, active: false})
+            break
+        default:
+            return
     }
-    _onInputEntered(text, disposition) {
-        browser.tabs.create
-        console.log(text, disposition)
-    }
-    _onInputCancelled() {
-    }
-}
-new omnibox()
+})
